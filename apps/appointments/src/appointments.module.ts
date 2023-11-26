2bd7dc82f732e0032b940df8065587044f0fde67
Anıl Gündal <a.gundal92@gmail.com>
2023-11-26 23:15:36 +0300

    passport added

--- apps/appointments/src/appointments.module.ts
+++ apps/appointments/src/appointments.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
-import { DatabaseModule, LoggerModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from "@app/common";
 import { AppointmentsRepository } from './appointments.repository';
 import {
   AppointmentDocument,
   AppointmentSchema,
 } from './models/appointment.schema';
+import { pinoHttp } from "pino-http";
+import { AuthModule } from "../../auth/src/auth.module";
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from  'joi';
 
 @Module({
   imports: [
@@ -15,6 +19,13 @@ import {
       { name: AppointmentDocument.name, schema: AppointmentSchema },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      })
+    })
   ],
   controllers: [AppointmentsController],
   providers: [AppointmentsService, AppointmentsRepository],
--- apps/appointments/src/main.ts
+++ apps/appointments/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppointmentsModule } from './appointments.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppointmentsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // If set to true validator will strip validated object of any properties that do not have any decorators.
   app.useLogger(app.get(Logger));
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT'));
 }
 
 bootstrap();
