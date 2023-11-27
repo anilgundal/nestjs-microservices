1b2f7fd84b02b4e3c245229fa58e0a51d35e16a9
Anıl Gündal <a.gundal92@gmail.com>
2023-11-27 22:28:33 +0300

    strategy added

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
@@ -7,10 +8,23 @@ export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async create(createUserDto: CreateUserDto) {
-    return this.usersRepository.create(createUserDto);
+    return this.usersRepository.create({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    });
   }
 
-  async getUsers() {
-    return this.usersRepository.find({});
+  async validateUser(email: string, passport: string){
+    const user = await this.usersRepository.findOne({email});
+    const passportIsValid = await bcrypt.compare(passport, user.password);
+
+    if(!passportIsValid) {
+      throw new UnauthorizedException('Kimlik bilgileri geçersiz!');
+    }
+
+    return user;
+
   }
+
+
 }
